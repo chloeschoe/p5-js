6fc65baae011f7e1a28b3b9f4ea204a3b33d468c
chloeschoe <chloesjchoe@gmail.com>
2017-03-20 00:29:18 -0700

    First-commit-js
    First commit

---
+++ basic/empty-example/sketch.js
@@ -0,0 +1,18 @@
+function setup() {
+
+	createCanvas (880, 880); 
+
+}
+
+function draw() {
+	if(mouseIsPressed) {
+		fill(0);
+	} else {
+		fill(255);
+	}
+
+	ellipse(mouseX,mouseY,80,80);
+	
+}
+
+
